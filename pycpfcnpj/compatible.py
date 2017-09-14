0713c197713f6afee698cb83aeea5e1d64837090
Diego Saraiva <diegosaraiva@gmail.com>
2017-09-14 09:34:14 -0300

    Update compatible.py
    Adjust to handle unicode string in Python 2.7

--- pycpfcnpj/compatible.py
+++ pycpfcnpj/compatible.py
@@ -3,8 +3,12 @@ import string
 
 
 def clear_punctuation(document):
-    """Remove from document all pontuation signals."""
-    if sys.version_info[0] < 3:
-        return document.translate(string.punctuation)
-    else:
-        return document.translate(str.maketrans("", "", string.punctuation))
+    document = str(document)
+    try:
+        """Remove from document all pontuation signals."""
+        if sys.version_info[0] < 3:
+            return document.translate(None,string.punctuation)
+        else:   
+            return document.translate(str.maketrans("", "", string.punctuation))
+    except:
+        return False;
