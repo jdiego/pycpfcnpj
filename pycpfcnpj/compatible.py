4b2eb3fbbaf7a4af01b81dbc2b86b3de1d97932c
Diego Saraiva <diegosaraiva@gmail.com>
2017-09-14 09:20:44 -0300

    Update compatible.py
    Remove extra field to Python 2 compatibility

--- pycpfcnpj/compatible.py
+++ pycpfcnpj/compatible.py
@@ -5,6 +5,6 @@ import string
 def clear_punctuation(document):
     """Remove from document all pontuation signals."""
     if sys.version_info[0] < 3:
-        return document.translate(None, string.punctuation)
+        return document.translate(string.punctuation)
     else:
         return document.translate(str.maketrans("", "", string.punctuation))
